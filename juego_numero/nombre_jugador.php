8de706f6d3be8f828f8625f858fb819963937fd6
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:18:38 +0200

    Update nombre_jugador.php

--- juego_numero/nombre_jugador.php
+++ juego_numero/nombre_jugador.php
@@ -1,12 +1,12 @@
 <?php
-$nombre_jugador = isset($_COOKIE['nombre_jugador']) ? $_COOKIE['nombre_jugador'] : '';
-
 if (isset($_POST['nombre'])) {
     $nombre_jugador = $_POST['nombre'];
     setcookie('nombre_jugador', $nombre_jugador, time() + 86400 * 30); // La cookie expira en 30 días
     header('Location: adivinanza.php');
     exit;
 }
+
+$nombre_jugador = isset($_COOKIE['nombre_jugador']) ? $_COOKIE['nombre_jugador'] : "";
 ?>
 
 <!DOCTYPE html>
@@ -16,10 +16,10 @@ if (isset($_POST['nombre'])) {
     <title>Nombre del jugador</title>
 </head>
 <body>
-    <h1><?= empty($nombre_jugador) ? 'Ingresa' : 'Edita' ?> tu nombre</h1>
+    <h1>Ingresa o edita tu nombre</h1>
     <form action="nombre_jugador.php" method="post">
         <input type="text" name="nombre" value="<?= $nombre_jugador ?>" required>
-        <button type="submit"><?= empty($nombre_jugador) ? 'Guardar' : 'Actualizar' ?></button>
+        <button type="submit">Guardar</button>
     </form>
 </body>
 </html>
