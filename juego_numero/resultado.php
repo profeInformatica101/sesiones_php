3f16fc8284053d90ee70a3965dfdd95c40c9345d
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:22:45 +0200

    Update resultado.php

--- juego_numero/resultado.php
+++ juego_numero/resultado.php
@@ -6,4 +6,28 @@ if (!$_SESSION['ganador'] && $_SESSION['intentos'] < 5) {
     exit;
 }
 
-$mensaje = $_SESSION['ganador'] ? '¡
+$mensaje = $_SESSION['ganador'] ? '¡Felicidades! Adivinaste el número.' : 'Lo siento, no adivinaste el número.';
+$nombre_jugador = $_COOKIE['nombre_jugador'];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+    <h1><?= $mensaje ?></h1>
+    <p>Jugador: <?= $nombre_jugador ?></p>
+    <p>Intentos realizados: <?= $_SESSION['intentos'] ?></p>
+    
+    <h2>Ganadores</h2>
+    <ul>
+        <?php foreach ($_SESSION['ganadores'] as $jugador => $intentos): ?>
+            <li><?= $jugador ?> - <?= $intentos ?> intento(s)</li>
+        <?php endforeach; ?>
+    </ul>
+    
+    <a href="inicio.php">Jugar de nuevo</a>
+</body>
+</html>
