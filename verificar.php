39447154832089099ba0b698f34e13e2bcaa405c
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 09:27:55 -0400

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -7,15 +7,27 @@ $contrasenia_correcta = "mi_contraseña_segura";
 // Verificar si el usuario proporcionó una contraseña
 if (isset($_POST['password'])) {
     $contrasenia_proporcionada = $_POST['password'];
-    
+
+    // Si no hay intentos registrados, inicializar el contador
+    if (!isset($_SESSION['intentos_fallidos'])) {
+        $_SESSION['intentos_fallidos'] = 0;
+    }
+
     // Comprobar si la contraseña proporcionada es correcta
-    if ($contrasenia_proporcionada === $contrasenia_correcta) {
+    if ($contrasenia_proporcionada === $contrasenia_correcta && $_SESSION['intentos_fallidos'] < 3) {
         $_SESSION['autenticado'] = true;
+        $_SESSION['tiempo_inicio'] = time();
         header('Location: area_privada.php');
         exit;
     } else {
         $_SESSION['autenticado'] = false;
-        header('Location: login.php?error=1');
+        $_SESSION['intentos_fallidos']++;
+        
+        if ($_SESSION['intentos_fallidos'] >= 3) {
+            header('Location: login.php?error=2');
+        } else {
+            header('Location: login.php?error=1');
+        }
         exit;
     }
 } else {
