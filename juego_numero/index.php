403f6a194069ffeed9758be4ae9ba43c5b2c69af
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:12:05 +0200

    Update index.php

--- juego_numero/index.php
+++ juego_numero/index.php
@@ -1,13 +1,7 @@
 <?php
-session_start();
-
-// Generar un número aleatorio y guardar en la sesión
-$_SESSION['numero_aleatorio'] = rand(1, 10);
-$_SESSION['intentos'] = 0;
-
-// Inicializar el puntaje del jugador usando una cookie
-if (!isset($_COOKIE['puntaje'])) {
-    setcookie('puntaje', 0, time() + 86400 * 30); // La cookie expira en 30 días
+if (!isset($_COOKIE['nombre_jugador'])) {
+    header('Location: nombre_jugador.php');
+    exit;
 }
 
 header('Location: adivinanza.php');
