5df40a530e7fa2955b260cf4a6a0e37243b65e25
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:19:22 +0200

    Update resultado.php

--- juego_numero/resultado.php
+++ juego_numero/resultado.php
@@ -1,31 +1,9 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['ganador']) && $_SESSION['intentos'] < 5) {
+if (!$_SESSION['ganador'] && $_SESSION['intentos'] < 5) {
     header('Location: adivinanza.php');
     exit;
 }
 
-$mensaje = $_SESSION['ganador'] ? '¡Felicidades! ¡Adivinaste el número!' : 'Lo siento, no adivinaste el número.';
-
-if ($_SESSION['ganador']) {
-    $nombre_jugador = $_COOKIE['nombre_jugador'];
-    $ganadores = isset($_SESSION['ganadores']) ? $_SESSION['ganadores'] : [];
-    $ganadores[] = ['nombre' => $nombre_jugador, 'intentos' => $_SESSION['intentos']];
-    $_SESSION['ganadores'] = $ganadores;
-}
-
-session_destroy();
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Resultado</title>
-</head>
-<body>
-    <h1><?= $mensaje ?></h1>
-    <a href="index.php">Jugar de nuevo</a> | <a href="nombre_jugador.php">Editar nombre</a>
-</body>
-</html>
+$mensaje = $_SESSION['ganador'] ? '¡
