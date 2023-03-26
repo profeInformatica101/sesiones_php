d750571f8505bf849d1e3872e443dcf9242d0b94
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:28:17 +0200

    Update adivinanza.php

--- juego_numero/adivinanza.php
+++ juego_numero/adivinanza.php
@@ -11,6 +11,8 @@ if ($_SESSION['ganador'] || $_SESSION['intentos'] >= 5) {
     exit;
 }
 
+$mensaje = "";
+
 if (isset($_POST['numero'])) {
     $_SESSION['intentos']++;
 
@@ -19,6 +21,10 @@ if (isset($_POST['numero'])) {
         $_SESSION['ganadores'][$_COOKIE['nombre_jugador']] = $_SESSION['intentos'];
         header('Location: resultado.php');
         exit;
+    } elseif ($_POST['numero'] < $_SESSION['numero_aleatorio']) {
+        $mensaje = "El número que ingresaste es más pequeño.";
+    } else {
+        $mensaje = "El número que ingresaste es más grande.";
     }
 }
 
@@ -35,10 +41,14 @@ $nombre_jugador = $_COOKIE['nombre_jugador'];
     <h1>Adivina el número, <?= $nombre_jugador ?>!</h1>
     <p>Tienes 5 intentos para adivinar un número entre 1 y 10.</p>
     <p>Intentos realizados: <?= $_SESSION['intentos'] ?></p>
+    
+    <?php if (!empty($mensaje)): ?>
+        <p><?= $mensaje ?></p>
+    <?php endif; ?>
+    
     <form action="adivinanza.php" method="post">
         <input type="number" name="numero" min="1" max="10" required>
         <button type="submit">Adivinar</button>
     </form>
 </body>
 </html>
-
