6a2e4398cd28b7e028881d1fbd426ad346ce2f5c
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 09:26:12 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h1>Iniciar sesión</h1>
+    <form action="verificar.php" method="post">
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password" required>
+        <button type="submit">Ingresar</button>
+    </form>
+</body>
+</html>
