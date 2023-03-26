24b1e377533c92edb165d27d1124f9d9c16f2dc3
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 23:05:42 +0200

    Create nombre_jugador.php

---
+++ juego_numero/nombre_jugador.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['nombre'])) {
+    $nombre_jugador = $_POST['nombre'];
+    setcookie('nombre_jugador', $nombre_jugador, time() + 86400 * 30); // La cookie expira en 30 días
+    header('Location: adivinanza.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Nombre del jugador</title>
+</head>
+<body>
+    <h1>Ingresa tu nombre</h1>
+    <form action="nombre_jugador.php" method="post">
+        <input type="text" name="nombre" required>
+        <button type="submit">Guardar</button>
+    </form>
+</body>
+</html>
