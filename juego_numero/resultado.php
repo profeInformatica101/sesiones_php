4aab4c3259bddff4047bb9437f0e0a137db6e859
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-26 22:52:39 +0200

    Create resultado.php

---
+++ juego_numero/resultado.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['ganador']) && $_SESSION['intentos'] < 5) {
+    header('Location: adivinanza.php');
+    exit;
+}
+
+$mensaje = $_SESSION['ganador'] ? '¡Felicidades! ¡Adivinaste el número!' : 'Lo siento, no adivinaste el número.';
+
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+    <h1><?= $mensaje ?></h1>
+    <p>Tu puntaje actual: <?= $_COOKIE['puntaje'] ?></p>
+    <a href="inicio.php">Jugar de nuevo</a>
+</body>
