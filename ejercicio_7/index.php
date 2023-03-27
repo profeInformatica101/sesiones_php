19f4658814551ba0887761b2691fb86225422058
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-27 12:46:25 +0200

    Update index.php

--- ejercicio_7/index.php
+++ ejercicio_7/index.php
@@ -1,1 +1,11 @@
+<?php
+session_start();
 
+if (!isset($_SESSION['oportunidades'])) {
+    $_SESSION['oportunidades'] = 4;
+    $_SESSION['password'] = '1234';
+}
+
+
+header('Location: juego.php');
+exit;
